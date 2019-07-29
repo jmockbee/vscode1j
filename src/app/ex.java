69e3d2f9ef644231123e05abfa66d4f4a0522974
Jon Mockbee <mockbee1000@gmail.com>
2019-07-29 16:26:59 -0400

    trying to use vs code

---
+++ src/app/ex.java
@@ -0,0 +1,7 @@
+package app;
+
+public class ex {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java part 2 remember you need to change the class");
+    }
+}
